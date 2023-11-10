46dcfe077ccd0472a96430ac030c6a77c410e04f
ashraful2430 <www.savage582@gmail.com>
2023-11-10 18:37:12 +0600

    added swiper for category section and make the website responsive

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -15,20 +15,20 @@ const Navbar = () => {
 
     return (
         <div>
-            <div className="navbar fixed z-10 bg-opacity-40 text-white bg-black max-w-screen-xl">
+            <div className="navbar fixed z-10 bg-opacity-40  bg-black max-w-screen-xl">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <div tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <div tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 lg:text-white shadow bg-base-100 rounded-box w-52">
                             {navLinks}
                         </div>
                     </div>
-                    <a className=" normal-case text-xl text-center"><span className="text-3xl font-black">Bistro Boss</span> <br /> <span className="text-2xl font-bold">Restaurant</span></a>
+                    <a className=" normal-case text-xl text-center text-white"><span className="lg:text-2xl font-black">Bistro Boss</span> <br /> <span className="lg:text-xl font-bold">Restaurant</span></a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <div className="menu menu-horizontal px-1">
+                    <div className="menu menu-horizontal px-1 lg:text-white">
                         {navLinks}
                     </div>
                 </div>
